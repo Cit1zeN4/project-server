f521ab961f9684ef6914155bafe1ce23b23739dd
Cit1zeN4 <set4433@gmail.com>
2020-05-07 20:59:28 +0300

    Added dbDefault

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = require('./src/route/router')
 const db = require('./src/db/database')
+const { roleDefault } = require('./src/db/dbDefault')
 require('dotenv').config()
 // Import all models to the cache before the database is synced.
 require('./src/model/importModels')
@@ -18,16 +19,17 @@ app.listen(port, () => {
   db.authenticate()
     .then(() => {
       console.log('Connection has been established successfully.')
+      db.sync({ logging: false })
+        .then(() => {
+          console.log('database was successfully synced')
+          // Adding default Roles
+          roleDefault()
+        })
+        .catch((err) => {
+          console.log('- error \n', err)
+        })
     })
     .catch((err) => {
       console.error('Unable to connect to the database:', err)
-    })
-
-  db.sync()
-    .then(() => {
-      console.log('database was successfully synced')
-    })
-    .catch((err) => {
-      console.log('- error \n', err)
     })
 })
